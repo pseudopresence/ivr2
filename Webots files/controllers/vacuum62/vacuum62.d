4e3825dbbcba5e93718072c2fa097bfb34c515d4
Chris Swetenham <cswetenham@gmail.com>
2011-11-11 15:08:32 +0000

    initial position

--- Webots files/controllers/vacuum62/vacuum62.d
+++ Webots files/controllers/vacuum62/vacuum62.d
@@ -1,7 +1,7 @@
 vacuum62.o: vacuum62.c \
  /opt/webots/include/controller/c/webots/differential_wheels.h \
- /opt/webots/include/controller/c/webots/types.h \
  /opt/webots/include/controller/c/webots/distance_sensor.h \
+ /opt/webots/include/controller/c/webots/types.h \
  /opt/webots/include/controller/c/webots/led.h \
  /opt/webots/include/controller/c/webots/receiver.h \
  /opt/webots/include/controller/c/webots/robot.h \
