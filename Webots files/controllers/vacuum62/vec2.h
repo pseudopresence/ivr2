a1440e1ff25f406ea6b7f88f6482b863b821595b
Chris Swetenham <cswetenham@gmail.com>
2011-11-21 18:24:41 +0000

    move some code to header files

---
+++ Webots files/controllers/vacuum62/vec2.h
@@ -0,0 +1,52 @@
+#ifndef _VEC2_H
+#define _VEC2_H
+
+class Vec2
+{
+public:
+  double m_x;
+  double m_y;
+
+  Vec2() : m_x(0), m_y(0) {}
+  Vec2(double const _x, double const _y) : m_x(_x), m_y(_y) {}
+  Vec2(Vec2 const& _v) : m_x(_v.m_x), m_y(_v.m_y) {}
+
+  Vec2 const& operator=(Vec2 const& _v) { m_x = _v.m_x; m_y = _v.m_y; return *this; }
+
+  double GetDir() const { return atan2(m_y, m_x); }
+  static Vec2 FromDir(double const _dir) { return Vec2(cos(_dir), sin(_dir)); }
+  
+  static Vec2 XAxis() { return Vec2(1, 0); }
+  static Vec2 YAxis() { return Vec2(0, 1); }
+
+  Vec2 RotatedBy(double const _theta) const
+  {
+    return Vec2(
+        m_x * cos(_theta) - m_y * sin(_theta),
+        m_x * sin(_theta) + m_y * cos(_theta)
+    );
+  }
+};
+
+inline Vec2 operator+(Vec2 const& _l, Vec2 const& _r)
+{
+  return Vec2(_l.m_x + _r.m_x, _l.m_y + _r.m_y);
+}
+
+inline Vec2 operator-(Vec2 const& _l, Vec2 const& _r)
+{
+  return Vec2(_l.m_x - _r.m_x, _l.m_y - _r.m_y);
+}
+
+inline Vec2 operator*(double const _s, Vec2 const& _v)
+{
+  return Vec2(_v.m_x * _s, _v.m_y * _s);
+}
+
+inline Vec2 operator*(Vec2 const& _v, double const _s)
+{
+  return _s * _v;
+}
+
+
+#endif // _VEC2_H
